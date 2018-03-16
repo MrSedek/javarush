d08d36cf89dc041d53386f13b1319a843cdf8082
MrSedek <sedek.by@gmail.com>
2018-03-16 17:07:18 +0300

    add_task1823

---
+++ task18/task1823/Solution.java
@@ -0,0 +1,23 @@
+﻿package com.javarush.task.task18.task1824;
+
+/* 
+Файлы и исключения
+*/
+
+import java.io.*;
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String s = "";
+        try {
+            while (true){
+                s = reader.readLine();
+                FileInputStream fis = new FileInputStream(s);
+                fis.close();
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println(s);
+        }
+    }
+}
