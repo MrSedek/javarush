790bcd5133cd13cc9ee2613f9b60f1bfffb41a4f
MrSedek <sedek.by@gmail.com>
2018-03-16 16:58:50 +0300

    add_task1822

---
+++ task18/task1822/Solution.java
@@ -0,0 +1,33 @@
+﻿package com.javarush.task.task18.task1822;
+
+/* 
+Поиск данных внутри файла
+*/
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String fileName1 = reader.readLine();
+
+        try (FileInputStream fis = new FileInputStream(fileName1)){
+            byte[] buffer = new byte[fis.available()];
+            fis.read(buffer, 0, buffer.length);
+            String text = new String(buffer);
+            String arg = args[0] + " ";
+            for (String retval : text.split("\n")) {
+                if (retval.startsWith(arg)){
+                    System.out.println(retval);
+                    return;
+                }
+            }
+        }
+        catch (Exception ex){
+            System.out.println(ex.getMessage());
+        }
+    }
+}
